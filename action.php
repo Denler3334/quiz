a4b068a49ce3a2570a14a5e3f9d598663d6fffa6
Nikolay Nikishin <43451550+gittitanweb@users.noreply.github.com>
2019-02-15 07:09:59 +0300

    +

---
+++ action.php
@@ -0,0 +1,23 @@
+<?php
+    
+    
+    /*echo json_encode([
+        'err' => '0',
+        'mess' => 'Квиз успешно пройден!',
+        'data' => $_POST
+    ]);
+    
+    echo "<pre>";
+    print_r( $_POST );
+    echo "</pre>";
+    
+    Array
+    (
+        [phone] => +79207224582
+        [first_name] => Николай
+        [position] => home
+        [quizdata] => [{"status":true,"question":"Какая у Вас сфера деятельности?","answer":"До 202"},{"status":true,"question":"Какое количество заказов Вы можете обработать в день?","answer":"До 202"},{"status":true,"question":"Сколько Вам полных лет","answer":"До 202"},{"status":true,"question":"22Какое количество заказов Вы можете обработать в день?","answer":"До 202"}]
+    )
+
+    */
+    
